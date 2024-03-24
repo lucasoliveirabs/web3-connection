7039736e91623b65da8fe8800d1408edd32417e0
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-03-24 00:36:39 -0300

    pilot commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
+import detectEthereumProvider from "@metamask/detect-provider";
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+import Web3 from "web3";
 
 function App() {
+  const walletAddress = "";
+  const setWalletAddress = "";
+  const buyAmount = "";
+  const sellAmount = "";
+  let carbonCoinAmount ="";
+  let web3 = "";
+
+  async function requestAccount(){
+    console.log('Requesting account');
+
+    if (window.ethereum) {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
+      web3 = initWeb3();
+    } catch (error) {
+      console.log('Error connecting..')
+    }
+
+  } else {
+    console.log("For the moment the use of MetaMask is required. Please, download it to your browser.");
+    //exibir modal
+  }
+  }
+  
+const handleBuyAmount = () => {
+    console.log('O valor final é: ', buyAmount);
+};
+
+const handleSellAmount = () => {
+  console.log('O valor final é: ', sellAmount);
+};
+
+const initWeb3 = async () => {
+  let web3 = null;
+  const metamaskProvider = await detectEthereumProvider();
+
+  if (metamaskProvider) {
+    web3 = new Web3(metamaskProvider);
+
+    metamaskProvider.on("chainChanged", reloadPage);
+    metamaskProvider.on("accountsChanged", reloadPage);   //login - redirect to home
+    metamaskProvider.on("disconnect", reloadPage);        //logout - redirect to login
+
+    const reloadPage = () => {
+      window.location.reload();
+    };
+  } else {
+    console.log("For the moment the use of MetaMask is required. Please, download it to your browser.");
+    //exibir modal
+  }
+  return web3;
+};
+
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button onClick={requestAccount}>Conectar carteira</button>
+        <h3>Endereço da carteira : {walletAddress}</h3>
       </header>
+
+      <div className="page-center">
+        {web3 !== null && (
+          <>
+            <h1 className="no-margin-top">Your CarbonCoin amount is:</h1>
+            <p className="no-margin">{carbonCoinAmount}</p>
+          </>
+        )}
+      </div>
+
+      {web3 !== null && (
+        <>
+          <form onSubmit={handleBuyAmount}>
+            <h1>How much CarbonCoins do you want to buy?</h1>
+            <div>
+              <label>CAC:</label>{" "}
+              <input value={buyAmount} />{" "}
+              <button className="btn primaryBtn" type="submit">
+                Enter
+              </button>
+            </div>
+          </form>
+        </>
+      )}
+
+      {web3 !== null && (
+        <>
+          <form onSubmit={handleSellAmount}>
+            <h1>How much CarbonCoins do you want to sell?</h1>
+            <div>
+              <label>CAC:</label>{" "}
+          <   input value={sellAmount}/>{" "}
+              <button className="btn primaryBtn" type="submit">
+                Enter
+              </button>
+            </div>
+          </form></>
+      )}      
     </div>
   );
 }
---
+++ src/web3Service.js
@@ -0,0 +1,266 @@
+import "/.config.js";
+
+const getTokenBalance = async () => {
+    try {
+      const address = await web3.eth.getAccounts();
+      const balance = await tokenContract.methods.balanceOf(address).call();
+      return balance;
+    } catch (error) {
+      return null;
+    }
+  };
+
+  const getNFTBalance = async () => {};
+
+  getTokenBalance().then(balance => {
+    //if null return error message, else return value
+  });
+
+  const purchaseTokens = async () => {
+    const contractAddress = '0xa3361c78C373cDb514717f736DAC5d0A2De00944'
+    const TokenAbi = [
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "name",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "string"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_spender",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "approve",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "totalSupply",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_from",
+                  "type": "address"
+              },
+              {
+                  "name": "_to",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "transferFrom",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "decimals",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint8"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [
+              {
+                  "name": "_owner",
+                  "type": "address"
+              }
+          ],
+          "name": "balanceOf",
+          "outputs": [
+              {
+                  "name": "balance",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "symbol",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "string"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_to",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "transfer",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [
+              {
+                  "name": "_owner",
+                  "type": "address"
+              },
+              {
+                  "name": "_spender",
+                  "type": "address"
+              }
+          ],
+          "name": "allowance",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "payable": true,
+          "stateMutability": "payable",
+          "type": "fallback"
+      },
+      {
+          "anonymous": false,
+          "inputs": [
+              {
+                  "indexed": true,
+                  "name": "owner",
+                  "type": "address"
+              },
+              {
+                  "indexed": true,
+                  "name": "spender",
+                  "type": "address"
+              },
+              {
+                  "indexed": false,
+                  "name": "value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "Approval",
+          "type": "event"
+      },
+      {
+          "anonymous": false,
+          "inputs": [
+              {
+                  "indexed": true,
+                  "name": "from",
+                  "type": "address"
+              },
+              {
+                  "indexed": true,
+                  "name": "to",
+                  "type": "address"
+              },
+              {
+                  "indexed": false,
+                  "name": "value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "Transfer",
+          "type": "event"
+      }
+  ]
+  
+    const tokenContractInstance = web3.eth.Contract(TokenAbi);
+    const approvedAddress = "0xa3361c78C373cDb514717f736DAC5d0A2De00944";
+    const bigNumberValue = web3.utils.toBN("1000000000000000000");
+    const myAddress = "0xC89d705104aFff28245e58C05AcfC82f91F2aEe9";
+    await tokenContractInstance.methods.approve(approvedAddress, bigNumberValue).send({ from: myAddress })
+        .then((receipt) => {
+            console.log("Transaction successful. Receipt:", receipt);
+        }).catch((error) => {
+            console.error("Transaction failed:", error);
+        });
+
+    // Call purchase function on token contract
+    //const tokenContractInstance = web3.eth.Contract(TokenAbi, tokenContractAddress);
+    //await tokenContractInstance.methods.buyTokens().send({ from: accounts[0], value: web3.utils.toWei('1', 'ether') });
+  };
+
+  const sellTokens = async () => {
+    const tokenContractInstance = new web3.eth.Contract(TokenAbi, tokenContractAddress);
+    // Call sell function on token contract
+    //await tokenContractInstance.methods.sellTokens(tokenAmount).send({ from: accounts[0] });
+  };
+
+
